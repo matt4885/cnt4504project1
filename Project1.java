b5dfa97603ab9dde06e95044f7df8f1151545f83
Matthew Kempey <mkempey@gmail.com>
2015-05-15 08:42:39 -0400

    Just basic main file

---
+++ Project1.java
@@ -0,0 +1,30 @@
+/*  
+ * CNT 4504 - COMPUTER NETWORKS AND DISTRIBUTED PROCESSING
+ * Project 1
+ * 
+ * Date Started: 05/14/2015
+ * 
+ * Group Machines Used
+ * Client:
+ * Server:
+ *
+ * *******************
+ * * Nicholas Hecht  *
+ * * Matthew Kempey  *
+ * * Casey           *
+ * * Braiden         *
+ * * Michael         *
+ * *******************
+ *
+ * Other Notes: 
+ *
+ *
+ */
+ 
+ 
+ public class Project1 {
+      public static void main(String[] args) {
+      
+      } //main
+ 
+ } //end class Project1
