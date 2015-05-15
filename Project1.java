8c29bb12316d6e2115208d0772e8d43e067ac9d0
Matthew Kempey <mkempey@gmail.com>
2015-05-15 09:09:00 -0400

    Added a switch menu system.

--- Project1.java
+++ Project1.java
@@ -20,10 +20,55 @@
  *
  *
  */
- 
+
+import java.io.*;
+import java.util.Scanner; 
  
  public class Project1 {
       public static void main(String[] args) {
+
+
+	/* System objects */
+	Scanner scan = new Scanner(System.in);
+
+
+	/* Local variables */ 
+	int choice;   //Used in switch statement for menu choice
+
+	while (true) {
+
+		switch (choice) {
+			case 1: 
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+			case 6:
+
+				break;
+			case 7:
+				System.out.println("Quitting...");
+
+				break;
+			default:
+				System.out.println("Invalid menu choice. Please enter 1-7.");
+			
+				break;
+		} //end of switch
+	
+
+
+	} //end of while loop
       
       } //main
  
