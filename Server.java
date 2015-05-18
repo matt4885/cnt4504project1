b6143c0beb27f1509df54c1759a782cc1712dfc1
matt4885 <mkempey@gmail.com>
2015-05-18 11:01:58 -0400

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,72 @@
+/*  
+ * UNIVERSITY OF NORTH FLORIDA COMPUTER SCIENCE
+ * CNT 4504 - COMPUTER NETWORKS AND DISTRIBUTED PROCESSING
+ * Project 1
+ * Group   5
+ * Date Started: 05/14/2015
+ * 
+ * Group Machines Used
+ * Client: 192.168.100.109
+ * Server: 192.168.100.110
+ * Port:   1234
+ * ********************
+ * * Nicholas Hecht   *
+ * * Matthew Kempey   *
+ * * Casey Dotson     *
+ * * Braden Weaver    *
+ * * Michael Williams * 
+ * ********************
+ *
+ * Other Notes: 
+ *
+ *
+ */
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Server {
+	public static void main(String[] args) {
+
+		/* System objects */
+		Scanner scan = new Scanner(System.in);
+
+		/* Local variables */
+		int choice; // Used in switch statement for menu choice
+
+		while (true) {
+
+			switch (choice) {
+			case 1:
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+			case 6:
+
+				break;
+			case 7:
+				System.out.println("Quitting...");
+				System.exit(1);
+				break;
+			default:
+				System.out.println("Invalid menu choice. Please enter 1-7.");
+
+				break;
+			} // end of switch
+
+		} // end of while loop
+
+	} // main
+
+} // end class Server
