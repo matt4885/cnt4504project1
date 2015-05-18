a807f12cc2e061838ceb84680ec135f8e4fa26a5
The1bman <the1bman.bw@gmail.com>
2015-05-18 16:35:35 -0400

    Update Server.java
    Established connection to client.

--- Server.java
+++ Server.java
@@ -1,80 +1,72 @@
-/*  
+/*
  * UNIVERSITY OF NORTH FLORIDA COMPUTER SCIENCE
  * CNT 4504 - COMPUTER NETWORKS AND DISTRIBUTED PROCESSING
  * Project 1
- * Group   5
+ * Group 5
  * Date Started: 05/14/2015
- * 
+ *
  * Group Machines Used
  * Client: 192.168.100.109
  * Server: 192.168.100.110
- * Port:   1234
+ * Port: 1234
  * ********************
- * * Nicholas Hecht   *
- * * Matthew Kempey   *
- * * Casey Dotson     *
- * * Braden Weaver    *
- * * Michael Williams * 
+ * * Nicholas Hecht *
+ * * Matthew Kempey *
+ * * Casey Dotson *
+ * * Braden Weaver *
+ * * Michael Williams *
  * ********************
  *
- * Other Notes: 
+ * Other Notes:
  *
  *
  */
-
 import java.io.*;
 import java.util.Scanner;
+import java.net.*;
 
-public class Server {
-	public static void main(String[] args) {
-
+public class server {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
 		/* System objects */
-		Scanner scan = new Scanner(System.in);
-
-		/* Local variables */
-		int choice; // Used in switch statement for menu choice
-
-		choice = 0; //will change to receive from client 
+		int portNumber = 1234; //Integer.parseInt(args[0]);
+		Socket clientSocket = null;
+		ServerSocket serverSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String inputLine = null;
+		String outputLine = null;
 		
+
+		System.out.println("Server listening on port " + portNumber + "...");
+
+	    serverSocket = new ServerSocket(portNumber);
+	    
 		while (true) {
-
-			switch (choice) {
-			/* Host current date and time */
-			case 1:
-
-				break;
-			/* Host uptime */
-			case 2:
 			
-				break;
-			/* Host memory use */
-			case 3:
-
-				break;
-			/* Host Netstat */
-			case 4:
-
-				break;
-			/* Host current users */
-			case 5:
-
-				break;
-			/* Host running processes */
-			case 6:
-
-				break;
-			case 7:
-				System.out.println("Quitting...");
-				System.exit(1);
-				break;
-			default:
-				System.out.println("Invalid menu choice. Please enter 1-7.");
-
-				break;
-			} // end of switch
-
-		} // end of while loop
-
+			clientSocket = serverSocket.accept();
+			System.out.println("  Client connection established");
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			
+			
+			try {
+				    out = new PrintWriter(clientSocket.getOutputStream(), true);
+				    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		            
+				    // Initiate conversation with client		    
+				    out.println("I see you");
+	
+				   /* while ((inputLine = in.readLine()) != null) {
+				        
+				        out.println(outputLine);
+				        if (outputLine.equals("Bye."))
+				            break;
+				    }*/
+			} 
+			catch (Exception ex){}
+		} // end while
+		
 	} // main
-
 } // end class Server
+	
