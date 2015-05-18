4d068cd43bdb84faa54db0bb5778c8b1d83cf562
Michael Williams <michael@sun-sol.com>
2015-05-18 11:11:31 -0400

    Test

--- Client.java
+++ Client.java
@@ -32,7 +32,7 @@ public class Client {
 		Scanner scan = new Scanner(System.in);
 
 		/* Local variables */
-		int choice; // Used in switch statement for menu choice
+		int choice; // Used in  switch statement for menu choice
 
 		while (true) {
 
