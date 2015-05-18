95ceb798e48d5776a4a13223602216b9058d3886
The1bman <the1bman.bw@gmail.com>
2015-05-18 17:14:40 -0400

    Update Server.java
    Added netstat, still need to fix output

--- Server.java
+++ Server.java
@@ -36,6 +36,10 @@ public class server {
 		BufferedReader in = null;
 		String inputLine = null;
 		String outputLine = null;
+		String s = null;
+		Process p = null;
+		BufferedReader stdInput;
+		int i = 0;
 		
 
 		System.out.println("Server listening on port " + portNumber + "...");
@@ -48,7 +52,53 @@ public class server {
 			System.out.println("  Client connection established");
 			out = new PrintWriter(clientSocket.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			String temp = in.readLine();
+			System.out.println(temp);
+			int choice = Integer.parseInt(temp);
 			
+			switch (choice) {
+			/* Host current date and time */
+			case 1:
+				p = Runtime.getRuntime().exec("date");
+				stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while((s = stdInput.readLine()) != null){
+					out.println(s);
+				}
+			break;
+			/* Host uptime */
+			case 2:
+			break;
+			/* Host memory use */
+			case 3:
+			break;
+			/* Host Netstat */
+			case 4:
+				p = Runtime.getRuntime().exec("netstat");
+				stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while((s = stdInput.readLine()) != null){
+					i++;
+					if(i == 81){
+						out.println();
+						i = 0;
+					}
+					out.print(s);
+				}
+				i = 0;
+			break;
+			/* Host current users */
+			case 5:
+			break;
+			/* Host running processes */
+			case 6:
+			break;
+			case 7:
+			System.out.println("Quitting...");
+			System.exit(1);
+			break;
+			default:
+			System.out.println("Invalid menu choice. Please enter 1-7.");
+			break;
+			} // end of switch
 			
 			try {
 				    out = new PrintWriter(clientSocket.getOutputStream(), true);
