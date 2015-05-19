35921068298506180788066954de51fe5e8e3c9a
matt4885 <mkempey@gmail.com>
2015-05-19 17:49:32 -0400

    Update Server.java
    Took out unused variables and general clean up.

--- Server.java
+++ Server.java
@@ -29,94 +29,101 @@ public class server {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
 		/* System objects */
-		int portNumber = 1234; //Integer.parseInt(args[0]);
+		int portNumber = 1234; // Integer.parseInt(args[0]);
 		Socket clientSocket = null;
 		ServerSocket serverSocket = null;
 		PrintWriter out = null;
 		BufferedReader in = null;
-		String inputLine = null;
-		String outputLine = null;
 		String s = null;
 		Process p = null;
 		BufferedReader stdInput;
-		int i = 0;
-		
 
 		System.out.println("Server listening on port " + portNumber + "...");
 
-	    serverSocket = new ServerSocket(portNumber);
-	    
+		serverSocket = new ServerSocket(portNumber);
+
 		while (true) {
-			
+
 			clientSocket = serverSocket.accept();
-			System.out.println("  Client connection established");
+			System.out.print("  Client connected: ");
 			out = new PrintWriter(clientSocket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
 			String temp = in.readLine();
-			System.out.println(temp);
+			// System.out.println(temp);
 			int choice = Integer.parseInt(temp);
-			
+
 			switch (choice) {
 			/* Host current date and time */
 			case 1:
+				System.out.println("'date' requested");
 				p = Runtime.getRuntime().exec("date");
-				stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				while((s = stdInput.readLine()) != null){
+				stdInput = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				while ((s = stdInput.readLine()) != null) {
 					out.println(s);
 				}
-			break;
+				break;
 			/* Host uptime */
 			case 2:
-			break;
+				System.out.println("'uptime' requested");
+				p = Runtime.getRuntime().exec("uptime");
+				stdInput = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				while ((s = stdInput.readLine()) != null) {
+					out.println(s);
+				}
+				break;
 			/* Host memory use */
 			case 3:
-			break;
+				System.out.println("'free' requested");
+				p = Runtime.getRuntime().exec("free");
+				stdInput = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				while ((s = stdInput.readLine()) != null) {
+					out.println(s);
+				}
+				break;
 			/* Host Netstat */
 			case 4:
+				System.out.println("'netstat' requested");
 				p = Runtime.getRuntime().exec("netstat");
-				stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				while((s = stdInput.readLine()) != null){
-					i++;
-					if(i == 81){
-						out.println();
-						i = 0;
-					}
-					out.print(s);
+				stdInput = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				while ((s = stdInput.readLine()) != null) {
+					out.println(s);
+
 				}
-				i = 0;
-			break;
+				break;
 			/* Host current users */
 			case 5:
-			break;
+				System.out.println("'who' requested");
+				p = Runtime.getRuntime().exec("who");
+				stdInput = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				while ((s = stdInput.readLine()) != null) {
+					out.println(s);
+				}
+				break;
 			/* Host running processes */
 			case 6:
-			break;
+				System.out.println("'ps -c' requested");
+				p = Runtime.getRuntime().exec("ps -c");
+				stdInput = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				while ((s = stdInput.readLine()) != null) {
+					out.println(s);
+				}
+				break;
 			case 7:
-			System.out.println("Quitting...");
-			System.exit(1);
-			break;
-			default:
-			System.out.println("Invalid menu choice. Please enter 1-7.");
-			break;
+				System.out.println("Quitting...");
+				System.exit(1);
+				break;
 			} // end of switch
-			
-			try {
-				    out = new PrintWriter(clientSocket.getOutputStream(), true);
-				    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		            
-				    // Initiate conversation with client		    
-				    out.println("I see you");
-	
-				   /* while ((inputLine = in.readLine()) != null) {
-				        
-				        out.println(outputLine);
-				        if (outputLine.equals("Bye."))
-				            break;
-				    }*/
-			} 
-			catch (Exception ex){}
+
+			out.println("\000\001\002"); // session termination sequence
+
 		} // end while
-		
+
 	} // main
 } // end class Server
-	
