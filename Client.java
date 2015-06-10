0e610ebc0998f75df0a8e10c5a3efda93645ee61
matt4885 <mkempey@gmail.com>
2015-06-10 17:42:45 -0400

    Update Client.java
    cleaned up client and added thread sleep command line argument

--- Client.java
+++ Client.java
@@ -21,58 +21,47 @@
  * args[0] = SERVER IP 
  * args[1] = SERVER PORT
  * args[2] = NUMBER OF CLIENTS
+ * args[3] = SLEEP TIME (TIME IN MS FOR MAIN TO WAIT FOR THREADS TO COMPLETE)
  */
 
 import java.io.*;
 import java.net.*;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Client implements Runnable{
-	private static Scanner scan;
+public class Client implements Runnable{	
+	private static Scanner input = new Scanner(System.in);
 	private static String serverIP = null;
 	private static int portNumber = 0;
 	private static long total = 0;
+	private static int choice = 0;
+	private static int sleepTime = 50;
 	
-	public static void main(String[] args) throws IOException {
-
-		String s = null;
+	public static void main(String[] args) throws IOException {		
 
 		System.out.println();
-		scan = new Scanner(System.in);
-		/* Local variables */
-		int choice = 0; // Used in switch statement for menu choice
-		byte[] addr = new byte[4]; // byte array to store the destination array
-		serverIP = args[0];
+		
+		/* Local variables */		
 		int numberOfClients = 0;
 		double average = 0;
-		// choice = scan.nextInt();
+		
 		/*
 		 * Grab the server IP
 		 */
+		serverIP = args[0];
+		
+		/*
+		 * Grab server port
+		 */
 		if (args.length > 1) {
 			try {
-				String tempString[] = args[0].split(".");
-				if (tempString.length == 4) {
-					for (int i = 0; i < tempString.length; i++) { // iterate
-						// through
-						// the
-						// server
-						// address
-						// array
-						byte b = Byte.parseByte(tempString[i]); // convert the
-						// octet to a
-						// byte
-						addr[i] = b; // store byte in appropriate addr array
-						// element
-					}
-				}
+				portNumber = Integer.parseInt(args[1]);
 			} catch (NumberFormatException e) {
-				System.err.println("Argument" + args[0]+ " must be an integer.");
+				System.err.println("Argument" + args[1]
+						+ " must be an integer.");
 				System.exit(1);
 			}
 		}
-
+		
 		/*
 		 * Grab the number of clients
 		 */
@@ -80,54 +69,63 @@ public class Client implements Runnable{
 			try {
 				numberOfClients = Integer.parseInt(args[2]);
 			} catch (NumberFormatException e) {
-				System.err.println("Argument" + args[1]
+				System.err.println("Argument" + args[2]
 						+ " must be an integer.");
 				System.exit(1);
 			}
 		}
-		portNumber = Integer.parseInt(args[1]);
-/*		do {
-			choice = userMenu();
-			if (choice >= 1 && choice <= 7) {
-				sendCmd(choice, portNumber, serverIP);
-				if (choice == 7)
-					System.exit(1);
+		
+		/*
+		 * Grab thread sleep time
+		 */
+		sleepTime = Integer.parseInt(args[3]);
+		if (args.length > 3) {
+			try {
+				sleepTime = Integer.parseInt(args[3]);
+			} catch (NumberFormatException e) {
+				System.err.println("Argument" + args[3]
+						+ " must be an integer.");
+				System.exit(1);
 			}
-
+		}
+		
+		do {
+			choice = userMenu();
+			if (choice >= 1 && choice < 7) {
+				Thread tclients[] = new Thread[numberOfClients];
+				for(int i = 0; i < numberOfClients; i++){
+					tclients[i] = new Thread(new Client());
+				}
+				
+				for(int j = 0; j< numberOfClients; j++){
+					tclients[j].start();
+				}				
+				
+				try {
+				    Thread.sleep(sleepTime);
+				} catch (InterruptedException e) {}   // pause for threads to send so the MRT print is after
+				
+				average = total/(double)numberOfClients; // calculate the mean response time
+				
+				System.out.println("\nThe mean response time: " + average +" ms"); // print MRT
+				average = 0;
+				total = 0;
+				choice = 0;
+			}
 		} while (choice != 7);
-		*/
-		Thread tclients[] = new Thread[numberOfClients];
-		for(int i = 0; i < numberOfClients; i++){
-			tclients[i] = new Thread(new Client());
-		}
-		
-		for(int j = 0; j< numberOfClients; j++){
-			tclients[j].start();
-		}
-		//(new Thread(new Client()).start();
-		
-		try {
-		    Thread.sleep(100);
-		} catch (InterruptedException e) {}   // pause for threads to send so the MRT print is after
-		
-		average = total/(double)numberOfClients; // calculate the mean response time
-		
-		System.out.println("\nThe mean response time: " + average); // print MRT
-		
+		System.out.println("Session terminated");
+		input.close();
+		System.exit(1);
 	} // main
 
-	private static void sendCmd(int choice, int portNumber, String serverIP)
-			throws IOException {
-		// TODO Auto-generated method stub
+	private static void sendCmd() throws IOException {		
 		PrintWriter out = null;
 		Socket kkSocket = null;
-		BufferedReader in = null;
-		int fromServer = 0;
+		BufferedReader in = null;		
 		String temp = "";
-		long t1, t2, tsub;
-		int count = 0;
+		long t1, t2, tsub;		
 
-		// try {
+		
 		kkSocket = new Socket(serverIP, portNumber);
 		out = new PrintWriter(kkSocket.getOutputStream(), true);
 		t1 = System.currentTimeMillis();
@@ -135,12 +133,8 @@ public class Client implements Runnable{
 		in = new BufferedReader(
 				new InputStreamReader(kkSocket.getInputStream()));
 		do {
-			if (choice != 7)
-				temp = in.readLine();
-			else
-				System.exit(1);
-
-			if (temp.equals("\000\001\002"))
+			temp = in.readLine();
+			if (temp.equals("\001\001\002"))
 				break;
 			else
 				System.out.print(temp + "\n");
@@ -149,14 +143,12 @@ public class Client implements Runnable{
 		kkSocket.close();
 		t2= System.currentTimeMillis();
 		tsub = t2 - t1;
-		System.out.println(tsub);
+		System.out.println("\nResponse time: " +tsub +" ms");
 		total = tsub + total;
 	} //sendCmd()
 
-	public static int userMenu() {
-		int choice = 0;
-		boolean parsable = false;
-		Scanner input = new Scanner(System.in);
+	public static int userMenu() {		
+		boolean parsable = true;		
 		String temp = "";
 		do {
 			parsable = true;
@@ -174,24 +166,23 @@ public class Client implements Runnable{
 
 			} catch (NumberFormatException e) {
 				System.out.println("Invalid input. Please try again (1-7).");
-			} 
-			finally {
 				parsable = false;
 			}
-		} while (parsable);
-		//input.close();
+			if (choice > 7 || choice <= 0)
+			{
+				System.out.println("Invalid input. Please try again (1-7).");
+				parsable = false;
+			}
+		} while (!parsable);		
 		return choice;
 	} // end userMenu()
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() {		
 		try {
-			sendCmd(1, portNumber, serverIP);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			sendCmd();
+		} catch (IOException e) {			
 			e.printStackTrace();
-		}
-		
+		}		
 	}
 } // end class Client
